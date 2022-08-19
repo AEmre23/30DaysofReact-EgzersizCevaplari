af9697fd01d56649fa910221f732d69b683ce7c0
AEmre23 <aemrea23@gmail.com>
2022-08-19 11:25:39 +0300

    holiday destination selector solved

--- Day_8_States.js
+++ Day_8_States.js
@@ -237,3 +237,346 @@ class App extends React.Component {
 }
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
+
+// Exercise Level 2-2
+// After long time of lock down, you may think of travelling and you do not know where to go. You may be interested to develop a random country selector that selects your holiday destination.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+const countries = [
+  {
+      "name": "Afghanistan",
+      "capital": "Kabul",
+      "languages": [
+          "Pashto",
+          "Uzbek",
+          "Turkmen"
+      ],
+      "population": 27657145,
+      "flag": "https://placeimg.com/220/180/any/grayscale",
+      "currency": "Afghan afghani"
+  },
+  {
+      "name": "Åland Islands",
+      "capital": "Mariehamn",
+      "languages": [
+          "Swedish"
+      ],
+      "population": 28875,
+      "flag": "https://placeimg.com/220/180/animals",
+      "currency": "Euro"
+  },
+  {
+      "name": "Albania",
+      "capital": "Tirana",
+      "languages": [
+          "Albanian"
+      ],
+      "population": 2886026,
+      "flag": "https://placeimg.com/220/180/any",
+      "currency": "Albanian lek"
+  },
+  {
+      "name": "Algeria",
+      "capital": "Algiers",
+      "languages": [
+          "Arabic"
+      ],
+      "population": 40400000,
+      "flag": "https://placeimg.com/220/180/nature",
+      "currency": "Algerian dinar"
+  },
+  {
+      "name": "American Samoa",
+      "capital": "Pago Pago",
+      "languages": [
+          "English",
+          "Samoan"
+      ],
+      "population": 57100,
+      "flag": "https://placeimg.com/220/180/people",
+      "currency": "United State Dollar"
+  },
+  {
+      "name": "Andorra",
+      "capital": "Andorra la Vella",
+      "languages": [
+          "Catalan"
+      ],
+      "population": 78014,
+      "flag": "https://placeimg.com/220/180/tech",
+      "currency": "Euro"
+  },
+  {
+      "name": "Angola",
+      "capital": "Luanda",
+      "languages": [
+          "Portuguese"
+      ],
+      "population": 25868000,
+      "flag": "https://placeimg.com/220/180/arch/grayscale",
+      "currency": "Angolan kwanza"
+  },
+  {
+      "name": "Anguilla",
+      "capital": "The Valley",
+      "languages": [
+          "English"
+      ],
+      "population": 13452,
+      "flag": "https://placeimg.com/220/180/arch",
+      "currency": "East Caribbean dollar"
+  },
+  {
+      "name": "Antarctica",
+      "capital": "",
+      "languages": [
+          "English",
+          "Russian"
+      ],
+      "population": 1000,
+      "flag": "https://placeimg.com/220/180/any/sepia",
+      "currency": "Australian dollar"
+  },
+  {
+      "name": "Antigua and Barbuda",
+      "capital": "Saint John's",
+      "languages": [
+          "English"
+      ],
+      "population": 86295,
+      "flag": "https://placeimg.com/220/180/nature/grayscale",
+      "currency": "East Caribbean dollar"
+  },
+  {
+    "name": "Argentina",
+    "capital": "Buenos Aires",
+    "languages": [
+        "Spanish",
+        "Guaraní"
+    ],
+    "population": 43590400,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Argentine peso"
+},
+{
+    "name": "Armenia",
+    "capital": "Yerevan",
+    "languages": [
+        "Armenian",
+        "Russian"
+    ],
+    "population": 2994400,
+    "flag": "https://placeimg.com/220/180/nature/sepia",
+    "currency": "Armenian dram"
+},
+{
+    "name": "Aruba",
+    "capital": "Oranjestad",
+    "languages": [
+        "Dutch",
+        "(Eastern) Punjabi"
+    ],
+    "population": 107394,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Aruban florin"
+},
+{
+    "name": "Australia",
+    "capital": "Canberra",
+    "languages": [
+        "English"
+    ],
+    "population": 24117360,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Australian dollar"
+},
+{
+    "name": "Austria",
+    "capital": "Vienna",
+    "languages": [
+        "German"
+    ],
+    "population": 8725931,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Euro"
+},
+{
+    "name": "Azerbaijan",
+    "capital": "Baku",
+    "languages": [
+        "Azerbaijani"
+    ],
+    "population": 9730500,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Azerbaijani manat"
+},
+{
+    "name": "Bahamas",
+    "capital": "Nassau",
+    "languages": [
+        "English"
+    ],
+    "population": 378040,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Bahamian dollar"
+},
+{
+    "name": "Bahrain",
+    "capital": "Manama",
+    "languages": [
+        "Arabic"
+    ],
+    "population": 1404900,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Bahraini dinar"
+},
+{
+    "name": "Bangladesh",
+    "capital": "Dhaka",
+    "languages": [
+        "Bengali"
+    ],
+    "population": 161006790,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Bangladeshi taka"
+},
+{
+    "name": "Barbados",
+    "capital": "Bridgetown",
+    "languages": [
+        "English"
+    ],
+    "population": 285000,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Barbadian dollar"
+},
+{
+    "name": "Belarus",
+    "capital": "Minsk",
+    "languages": [
+        "Belarusian",
+        "Russian"
+    ],
+    "population": 9498700,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "New Belarusian ruble"
+},
+{
+    "name": "Belgium",
+    "capital": "Brussels",
+    "languages": [
+        "Dutch",
+        "French",
+        "German"
+    ],
+    "population": 11319511,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Euro"
+},
+{
+    "name": "Belize",
+    "capital": "Belmopan",
+    "languages": [
+        "English",
+        "Spanish"
+    ],
+    "population": 370300,
+    "flag": "https://placeimg.com/220/180/nature/grayscale",
+    "currency": "Belize dollar"
+}
+]
+const Header = () => (
+  <header style={{  backgroundColor:'aqua', padding:'10px 0'}}>
+    <div style={{fontSize:'42px', fontWeight:'800',  marginBottom:'20px', textAlign:'center'}}>
+      30 Days of React
+    </div>
+    <div style={{fontSize:'33px', fontWeight:'600',  marginBottom:'10px', textAlign:'center'}}>
+      Country Selector
+    </div>
+    <div style={{fontSize:'13px', fontWeight:'500',  marginBottom:'10px', textAlign:'center'}}>
+    Holiday Destination 
+    </div>
+  </header>
+)
+const Footer = () => (
+  <footer style={{position:'absolute', bottom:'0', width:'100%', textAlign:'center', backgroundColor:'aqua',padding:'10px 0', fontWeight:'600' }}>
+    by Emre ALTUNKAYA
+  </footer>
+)
+
+function commify(n) {
+  var parts = n.toString().split(".");
+  const numberPart = parts[0];
+  const decimalPart = parts[1];
+  const thousands = /\B(?=(\d{3})+(?!\d))/g;
+  return numberPart.replace(thousands, ",") + (decimalPart ? "." + decimalPart : "");
+  } 
+const rowStyle= {marginBottom:'10px'}
+class Country extends React.Component {
+  state ={
+    flag: 'https://placeimg.com/220/180/any',
+    name: 'Afghanistan',
+    capital: 'Kabul',
+    languages: 'Pashto,Uzbek,Turkmen',
+    population: commify(27657145),
+    currency: "Afghan afghani"
+  } 
+  changeCountry = () => {
+    let deger = this.props.ulkeler
+    let index = Math.floor(Math.random()* deger.length)
+    let langs = deger[index].languages
+    this.setState({
+      flag: deger[index].flag,
+      name: deger[index].name,
+      capital: deger[index].capital,
+      population: commify(deger[index].population),
+      languages: langs.join(', '),
+      currency: deger[index].currency
+    })
+  }
+  render(){
+    const {ulkeler} = this.props.ulkeler
+    return (
+      <>
+        <div style={{padding:'30px 60px', border:'1px solid whitesmoke', boxShadow:'rgb(14 30 37 / 12%) 0px 2px 4px 0px, rgb(14 30 37 / 32%) 0px 2px 16px 0px'}}>
+          <div style={{minWidth:'350px', marginBottom:'20px', display:'flex',flexDirection:'column',justifyContent:'Center', alignItems:'center'}}>
+          <img src={this.state.flag} alt={this.state.name} />
+          <div style={{fontWeight:'600',marginTop:'10px', fontSize:'22px'}}>{this.state.name}</div>
+          </div>
+
+          <div style={rowStyle}><span style={{fontWeight:'600'}}>Capital:</span> {this.state.capital}</div>
+          <div style={rowStyle}><span style={{fontWeight:'600'}}>Languages:</span> {this.state.languages}</div>
+          <div style={rowStyle}><span style={{fontWeight:'600'}}>Population:</span> {this.state.population}</div>
+          <div style={rowStyle}><span style={{fontWeight:'600'}}>Currency:</span> {this.state.currency}</div>
+        </div>
+        <button onClick={this.changeCountry} style={{cursor:'pointer',padding:'10px 30px', backgroundColor:'aqua', marginTop:'25px', border:'none', borderRadius:'5px', color:'white', fontWeight:'600'}} >
+            Select Country
+        </button>
+      </>
+    )
+  }
+}
+class Main extends React.Component {
+  render() {
+    return (
+      <main style= {{margin:'30px 0',width:'100%', display:'flex',flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+      <Country ulkeler={countries} />
+
+      </main>
+    )
+  }
+}
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{fontFamily:'Arial', position:'relative', minHeight:'100vh'}}>
+      <Header />
+      <Main />
+      <Footer />
+      </div>
+    )
+  }
+}
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
