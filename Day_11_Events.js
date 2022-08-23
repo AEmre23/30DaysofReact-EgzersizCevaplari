3df2d3606baf674596744f23b968a7fb1c98289f
AEmre23 <aemrea23@gmail.com>
2022-08-23 20:32:47 +0300

    day11 exercises completed

--- Day_11_Events.js
+++ Day_11_Events.js
@@ -34,3 +34,53 @@ export default App
 const rootElement = document.getElementById('root')
 // we render the JSX element using the ReactDOM package
 ReactDOM.render(<App />, rootElement)
+
+
+
+// ------------------------------------------------------------------------------------------ //
+// Exercises Level 2
+// Implement the following using onMouseEnter event
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  state={
+    top:'30%',
+    left:'50%',
+    display1:'block',
+    display2:'none'
+  }
+
+  handleMouseMove = event => {
+    let screenHeight = document.querySelector('.Screen').scrollHeight-40
+    let screenWidth = document.querySelector('.Screen').scrollWidth-120
+    let randomX = Math.floor(Math.random()*screenWidth)
+    let randomY = Math.floor(Math.random()*screenHeight)
+    //the first one is make 0,0 point for the div
+    this.setState({ top: randomY, left:randomX})
+  };
+  congratzClick = () => {
+    this.setState({display1:'none', display2:'flex'})
+  }
+
+
+  render() {
+    return(
+      <div className='Screen' style={{position:'relative',display:'flex',justifyContent:'center', alignItems:'center',width:'100%', height:'100vh'}} >
+        <div onMouseOver={this.handleMouseMove} style={{position:'absolute', top:this.state.top, left:this.state.left}}>
+          <button onClick={this.congratzClick} style={{display:this.state.display1,color:'white',border:'1px solid black',borderRadius:'10px', padding:'10px',backgroundColor:'grey'}}>
+          Click and win 1M $ Dollar
+          </button>
+        </div>
+        <div style={{display:this.state.display2, justifyContent:'center',alignItems:'center'}}>
+            Congratz!
+          </div>
+      </div>
+    )
+  }
+}
+export default App
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(<App />, rootElement)
