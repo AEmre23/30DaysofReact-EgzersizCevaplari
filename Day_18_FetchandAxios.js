02fc0a05de991171e76747652589b18a0df794f8
AEmre23 <aemrea23@gmail.com>
2022-08-28 14:26:49 +0300

    I also added route to API exercise

--- Day_18_FetchandAxios.js
+++ Day_18_FetchandAxios.js
@@ -1,9 +1,35 @@
 // Exercise Level 2
 // Find the average metric weight and life span of cats in the following API(https://api.thecatapi.com/v1/breeds).
 // There are 67 breeds of cats in the API.
+// I also use react route in this exercise!
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
 import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom'
+
+
+const CatData = ({allCats}) =>{
+
+  let loop = allCats.map((cat,index) => {
+
+  let tag = 'rd'
+  if (index+1 == 1) tag = 'st'
+  else if (index+1 == 2) tag= 'nd'
+  else tag='rd'
+  return <p>{index+1}{tag} cat's name is {cat.name}. Its life span is between {cat.life_span} years and weight is betweeen {cat.weight.metric} kg. </p>
+  })
+
+  return (
+  <>
+  <h2>There are {allCats.length} cat breeds in this data. Here is every one of them:</h2>
+  <p> You can go <NavLink to='/'>Home</NavLink> from here.</p>
+  {loop}
+  </>
+)}
 
 const Cats = ({ allCats, catLength }) => {
   let TotalWeigth = 0
@@ -15,11 +41,11 @@ const Cats = ({ allCats, catLength }) => {
   let catLoop = allCats.map((cat) => {
 
   let metricWeigth = cat.weight.metric
-  let digits = metricWeigth.replace(/\D/g, ""); // to avoid "-" in object
+  let digits = metricWeigth.replace(/\D/g, "");
 
   if (digits.length > 2){
     //if its like 4-11 kg ,which make them 3 length,
-    // it makes  411 so we have to seperate second number like 4 and 11 to sum up.
+    // it makes  411 so we have to seperate second number like 4 11 to sum up.
     let number2 = digits[1]+digits[2] 
     let sumOfCat = (parseInt(digits[0]) + parseInt(number2))/2
     TotalWeigth += sumOfCat
@@ -51,6 +77,11 @@ const Cats = ({ allCats, catLength }) => {
     averageLifespan = (TotalLifespan/catLength).toFixed(2)
 })
   return (
+    <>
+    <h1>React Component Life Cycle</h1>
+    <h1>Calling API</h1>
+    <p>I use this <a target='_blank' href='https://api.thecatapi.com/v1/breeds'>API link</a> to get data.</p>
+    <p>There are {catLength} cat breed in the api</p>
     <div className='cats'>
       <div className='weight' style={{display:'flex', gap:'20px',marginBottom:'20px'}}>
         <p>average metric weight :</p><p style={{color:'red'}}>{metricAverageWeight} kg</p> 
@@ -59,6 +90,8 @@ const Cats = ({ allCats, catLength }) => {
         <p>average cat lifespan :</p><p style={{color:'red'}}>{averageLifespan} year</p> 
       </div>
     </div>
+    <p>You can check every cat from <NavLink to='/catdata'>here</NavLink></p>
+    </>
   )
 }
 
@@ -86,17 +119,40 @@ class App extends Component {
     let catData = this.state.data
     let numberOfCatBreeds = this.state.data.length
     return (
+      // <div className='App'>
+      //   <h1>React Component Life Cycle</h1>
+      //   <h1>Calling API</h1>
+      //   <div>
+      //     <p>There are {numberOfCatBreeds} cat breed in the api</p>
+      //     <div className='countries-wrapper'>
+      //         <
+      //         
+      //     </div>
+      //   </div>
+      // </div>
+      <Router>
       <div className='App'>
-        <h1>React Component Life Cycle</h1>
-        <h1>Calling API</h1>
-        <div>
-          <p>There are {numberOfCatBreeds} cat breed in the api</p>
-          <div className='countries-wrapper'>
-              <Cats allCats={catData} catLength={numberOfCatBreeds} />
-          </div>
-        </div>
+        <Switch>
+          <Route 
+            exact path='/' 
+            component={(props) => (
+            <Cats
+            allCats={catData} 
+            catLength={numberOfCatBreeds}
+            /> )}
+           />
+          <Route 
+            path='/catdata' 
+            component={(props) => (
+            <CatData
+            allCats={catData} 
+            />)} 
+          />
+        </Switch>
       </div>
+    </Router>
     )
   }
 }
+
 export default App
